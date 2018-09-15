9b7a63aea6231751801f7b7f1e4b1af47a7df7b7
shoehead0125 <shoehead0125@gmail.com>
2018-09-15 21:14:38 +0900

    P6 Create movie.rb

---
+++ chapter_1/movie.rb
@@ -0,0 +1,21 @@
+class Movie < Entity
+  def initialize(ident)
+    super 'movies', ident
+  end
+
+  def title
+    get 'title'
+  end
+
+  def title=(value)
+    set 'title', value
+  end
+  
+  def director
+    get 'director'
+  end
+
+  def director=(value)
+    set 'director', value
+  end
+end
